1af9b503c4324ef3429f2aedd7353564fda0d42f
bikkimahato <bikkimahato123@gmail.com>
2020-10-16 14:53:41 +0530

    Event Acknowledgment and Message Rendering

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const http = require('http')
 
 const express = require('express')
 const socketio = require('socket.io')
+const Filter = require('bad-words')
 
 const app = express()
 const server = http.createServer(app)
@@ -19,12 +20,20 @@ io.on('connection', (socket) => {
     socket.emit('message', 'Welcome!')
     socket.broadcast.emit('message', 'A new user has joined')
 
-    socket.on('sendMessage', message => {
+    socket.on('sendMessage', (message, callback) => {
+        const filter = new Filter()
+
+        if (filter.isProfane(message)) {
+            return callback('Profanity is not allowed!')
+        }
+
         io.emit('message', message)
+        callback('Delivered')
     })
 
-    socket.on('sendLocation', coords => {
-        io.emit('message', `https://google.com/maps?q=${coords.latitude},${coords.longitude}`)
+    socket.on('sendLocation', (coords, callback) => {
+        io.emit('locationMessage', `https://google.com/maps?q=${coords.latitude},${coords.longitude}`)
+        callback()
     })
 
     socket.on('disconnect', () => {
